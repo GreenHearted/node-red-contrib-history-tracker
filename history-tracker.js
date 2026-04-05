9e8d0c2868365348d704899fef597b221b9aa2bf
andre <andreschey@posteo.de>
2026-04-05 18:29:57 +0200

    bugfix setting max value of gauge

--- history-tracker.js
+++ history-tracker.js
@@ -95,14 +95,19 @@ function NodeREDModule(RED) {
         }
         
         // Helper function to build the gauge message for output 1 (current month value + goal as max)
+        // msg.payload      - current month value (Dashboard 1 & 2)
+        // msg.max          - current month goal as max (Dashboard 2)
+        // msg.ui_control   - { max: goal } for Dashboard 1 gauge
         const buildGaugeMsg = function(data, originalMsg) {
             const gaugeMsg = RED.util.cloneMessage(originalMsg);
             gaugeMsg.payload = data.currentMonth && data.currentMonth.value !== undefined
                 ? data.currentMonth.value
                 : null;
-            gaugeMsg.max = data.currentMonth && data.currentMonth.goal !== undefined
+            const goalMax = data.currentMonth && data.currentMonth.goal !== undefined
                 ? data.currentMonth.goal
                 : null;
+            gaugeMsg.max = goalMax;
+            gaugeMsg.ui_control = { max: goalMax };
             gaugeMsg.period = data.currentMonth && data.currentMonth.period
                 ? data.currentMonth.period
                 : null;
